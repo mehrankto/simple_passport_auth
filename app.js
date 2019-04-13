9efc8689f27baea400e1924acd0e31d0dc2e24da
M.n.M <m.n.m@Ms-MacBook-Pro.local>
2019-04-13 19:23:57 +0430

    athenticating functionality (login, register, logout) added.

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
 const mongoose = require('mongoose');
+const flash = require('connect-flash');
+const session = require('express-session');
+const passport = require('passport');
 
 const app = express();
+
+//passport config
+require('./passport.js')(passport);
 
 //DB
 mongoose.connect('mongodb://localhost:27017/passport', {useNewUrlParser: true})
@@ -15,6 +21,28 @@ app.set('view engine', 'ejs');
 
 //BodyParser
 app.use(express.urlencoded({ extended: false }));
+
+//express session
+app.use(session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true
+}));
+
+//passport
+app.use(passport.initialize());
+app.use(passport.session());
+
+//connect flash
+app.use(flash());
+
+//global vars
+app.use((req, res, next) => {
+    res.locals.success_msg = req.flash('success_msg');
+    res.locals.error_msg = req.flash('error_msg');
+    res.locals.error = req.flash('error');
+    next();
+});
 
 //Routes
 app.use('/', require('./routes/index'));
--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
+const passport = require('passport');
+
+const User = require('../models/Users');
 
 //Login
 router.get('/login', (req,res) => res.render("login"));
@@ -34,10 +38,59 @@ router.post('/register', (req, res) => {
             email,
             password,
             password2
-        })
+        });
     } else {    //validation passes
-        res.send('pass')
+        User.findOne({ email: email })
+        .then( user => {
+            if (user) {
+                errors.push({ msg: 'email is already registered'});
+                res.render('register', {
+                    errors,
+                    name,
+                    email,
+                    password,
+                    password2
+                });
+            } else {
+                const newUser = new User({
+                    name,
+                    email,
+                    password,
+                });
+
+                //hashing
+                bcrypt.genSalt(10, (err, salt) => 
+                    bcrypt.hash(newUser.password, salt, (err, hash) => {
+                        if(err) throw err;
+
+                        newUser.password = hash;
+                        //save user
+                        newUser.save()
+                            .then(user => {
+                                req.flash('success_msg', 'you are now registered and can log in');
+                                res.redirect('/users/login')
+                            })
+                            .catch(err => console.log(err));
+                }));
+            }
+        });
     }
 });
+
+//login handle
+router.post('/login', (req, res, next) => {
+    passport.authenticate('local', {
+        successRedirect: '/dashboard',
+        failureRedirect: '/users/login',
+        failureFlash: true
+    })(req, res, next);
+});
+
+//logout handle
+router.get('/logout', (req, res) => {
+    req.logOut();
+    req.flash('success_msg', 'logged out');
+    res.redirect('/users/login');
+})
 
 module.exports = router;
